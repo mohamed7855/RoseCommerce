43130294b3b9f928ca542e4bf934c19dc332f882
mahbahig <mahbahig2@gmail.com>
2025-01-21 04:12:46 +0200

    Added footer and routes and needed pages to the project, add main colors to the tailwind configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,10 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./core/pages/home/home.component').then((c) => c.HomeComponent),
   },
+  {path: 'about', title: 'About Us', loadComponent: () => import('./core/pages/about/about.component').then(m => m.AboutComponent)},
+  {path: 'location', title: 'About Us', loadComponent: () => import('./core/pages/location/location.component').then(m => m.LocationComponent)},
+  {path: 'contact', title: 'Contact Us', loadComponent: () => import('./core/pages/contact/contact.component').then(m => m.ContactComponent)},
+  {path: 'delivery', title: 'Delivery', loadComponent: () => import('./core/pages/delivery/delivery.component').then(m => m.DeliveryComponent)},
+  {path: 'policy', title: 'Policy', loadComponent: () => import('./core/pages/policy/policy.component').then(m => m.PolicyComponent)},
+  {path: 'faqs', title: 'FAQS', loadComponent: () => import('./core/pages/faqs/faqs.component').then(m => m.FaqsComponent)}
 ];
