edad261382810b04e469b3d64eb21b32038d5328
mahbahig <mahbahig2@gmail.com>
2025-01-22 22:20:29 +0200

    Created navbar and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./core/pages/home/home.component').then((c) => c.HomeComponent),
   },
+  { path: 'category', title: 'Category', loadComponent: () => import('./core/pages/category/category.component').then((c) => c.CategoryComponent) },
+  { path: 'about', title: 'About Us', loadComponent: () => import('./core/pages/about/about.component').then((c) => c.AboutComponent) },
+  { path: 'contact', title: 'Contact Us', loadComponent: () => import('./core/pages/contact/contact.component').then((c) => c.ContactComponent) },
 ];
