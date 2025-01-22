1c905e20df2cdbca29b78398fec9db91f86ed376
mahbahig <mahbahig2@gmail.com>
2025-01-22 21:47:08 +0200

    Downloaded needed assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBg: '#F82BA9',
+        secondBg: '#FEEDF7',
+        thirdBg: '#8C52FF',
+        mainText: '#160E4B',
+        secondText: '#757F95',
+        thirdText: '#F05454',
+        fourthText: '#DEE2E6'
+      }
+    },
   },
   plugins: [],
 };
