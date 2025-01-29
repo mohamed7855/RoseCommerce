8a3328b5f24286cf7fb8591d3cc03ea03d2307e1
Farouk-Zaazaa <farouk.route@gmail.com>
2025-01-29 14:24:28 +0200

    looping on a list of images

--- src/app/feature/components/trusted-by/trusted-by.component.ts
+++ src/app/feature/components/trusted-by/trusted-by.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class TrustedByComponent {
 
+  imgList:string[]=[
+    "./images/image1.png",
+    "./images/image2.png",
+    "./images/image3.png",
+    "./images/image4.png",
+    "./images/image5.png",
+    "./images/image6.png",
+  ]
+
 }
