ff5136d321c4ff45d3e85d08be9dbd5be31fd080
Farouk-Zaazaa <farouk.route@gmail.com>
2025-02-02 15:47:03 +0200

    static-info done

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SpecialGiftComponent } from "../special-gift/special-gift.component";
 import { CategoriesSliderComponent } from "../../components/categ-slider/categ-slider.component";
+import { StaticInfoComponent } from "../../components/static-info/static-info.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SpecialGiftComponent, CategoriesSliderComponent],
+  imports: [SpecialGiftComponent, CategoriesSliderComponent, StaticInfoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {},
     container: {
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
+      center: true,
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1600px',
+        },
     },
   },
   plugins: [],
