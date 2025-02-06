f964e1e553306b5e08e5021f1d704458815e5f1c
Farouk-Zaazaa <farouk.route@gmail.com>
2025-02-06 13:29:30 +0200

    modal done

---
+++ src/app/feature/components/modal/modal.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, inject, Input, OnChanges, Output, Renderer2, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent implements OnChanges {
+  private readonly renderer2 = inject(Renderer2)
+
+  @Input() isOpen: boolean = false;
+  @Output() modalClose: EventEmitter<void> = new EventEmitter<void>();
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isOpen']) {
+      if (this.isOpen) {
+        this.renderer2.addClass(document.body, 'overflow-hidden');
+      } else {
+        this.renderer2.removeClass(document.body, 'overflow-hidden');
+      }
+    }
+  }
+
+  closeModal(): void {
+    this.isOpen = false;
+    this.modalClose.emit();
+  }
+
+
+  // lw had hy reuse el component da mhtag t3ml 2 functions fel comp wahed open w wahd close w dol byghyro el stat bta3 boolean variable 
+  // example:
+
+  // isClicked:boolean = false;
+  // openModal():void{
+  //   this.isClicked = true;
+  // }
+  // closeModal():void{
+  //   this.isClicked = false;
+  // }
+}
