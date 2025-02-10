19c6e436b4797a1dfb950c9d9f40909c5ea6665f
mahbahig <mahbahig2@gmail.com>
2025-02-10 23:47:20 +0200

    Merge of dev into navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    title: 'Home',
-    loadComponent: () =>
-      import('./feature/pages/home/home.component').then((c) => c.HomeComponent),
-  },
+  { path: 'home', title: 'Home', loadComponent: () => import('./feature/pages/home/home.component').then((c) => c.HomeComponent) },
+  { path: 'category', title: 'Category', loadComponent: () => import('./core/pages/category/category.component').then((c) => c.CategoryComponent) },
+  { path: 'about', title: 'About Us', loadComponent: () => import('./feature/pages/about/about.component').then((c) => c.AboutComponent) },
+  { path: 'contact', title: 'Contact Us', loadComponent: () => import('./feature/pages/contact/contact.component').then((c) => c.ContactComponent) },
+  { path: 'location', title: 'Location', loadComponent: () => import('./feature/pages/location/location.component').then((c) => c.LocationComponent) },
+  { path: 'delivery', title: 'Delivery', loadComponent: () => import('./feature/pages/delivery/delivery.component').then((c) => c.DeliveryComponent) },
+  { path: 'policy', title: 'Policy', loadComponent: () => import('./feature/pages/policy/policy.component').then((c) => c.PolicyComponent) },
+  { path: 'faqs', title: 'FAQs', loadComponent: () => import('./feature/pages/faqs/faqs.component').then((c) => c.FaqsComponent) },
 ];
--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,9 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  showNavbar = signal(false);
+  toggleNavbar(): void {
+    this.showNavbar.set(!this.showNavbar());
+  }
 
 }
