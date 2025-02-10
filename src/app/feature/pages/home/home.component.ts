7dfb1937344ccb00acde591f8317ff34c81403a9
mohamed7855 <martm7726@gmail.com>
2025-02-10 17:36:29 +0200

    modal with register form

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { CustomerOpinionComponent } from '../../components/customer-opinion/cust
 import { TrustedByComponent } from '../../components/trusted-by/trusted-by.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
 import { ModalComponent } from "../../components/modal/modal.component";
+import { RegisterComponent } from "../../../core/pages/register/register.component";
 
 @Component({
   selector: 'app-home',
@@ -18,7 +19,8 @@ import { ModalComponent } from "../../components/modal/modal.component";
     TrustedByComponent,
     AboutUsComponent,
     GalleryComponent,
-    ModalComponent
+    ModalComponent,
+    RegisterComponent
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
